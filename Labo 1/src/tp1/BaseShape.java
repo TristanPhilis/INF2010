f31e6d39c91f9cecea7018e0a108e3256323db91
cravethedave <david.saikali@polymtl.ca>
2020-09-18 19:47:07 -0400

    BaseShape.java passes all tests

--- Labo 1/src/tp1/BaseShape.java
+++ Labo 1/src/tp1/BaseShape.java
@@ -1,5 +1,6 @@
 package tp1;
 
+import java.awt.*;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -11,44 +12,58 @@ public class BaseShape implements Cloneable {
         this.coords = new ArrayList<>();
     }
 
+
     // TODO prendre une liste de points et creer une nouvelle forme.
     public BaseShape(Collection<Point2d> coords) {
         this.coords = new ArrayList<>(coords);
     }
+
 
     // TODO ajouter ou retirer des coordonnees a la liste de points.
     public BaseShape add(Point2d coord) {
         this.coords.add(coord);
         return this;
     }
+
+
     public BaseShape add(BaseShape shape) {
         Collection<Point2d> temp = shape.getCoords();
         this.addAll(temp);
         return this;
     }
+
+
     public BaseShape addAll(Collection<Point2d> coords) {
         this.coords.addAll(coords);
         return this;
     }
+
+
     public BaseShape remove(Point2d coord) {
         coords.remove(coord);
         return this;
     }
+
+
     public BaseShape remove(BaseShape shape) {
         for(Point2d iter : shape.coords){
             remove(iter);
         }
         return this;
     }
+
+
     public BaseShape removeAll(Collection<Point2d> coords) {
         this.coords.removeAll(coords);
         return this;
     }
 
+
     // TODO retourner les coordonnees de la liste.
     public Collection<Point2d> getCoords() {
         return coords;
     }
+
 
     // TODO retourner une nouvelle liste ou tous les points sont des copy
     public Collection<Point2d> getCoordsDeepCopy() {
@@ -59,43 +74,82 @@ public class BaseShape implements Cloneable {
         return deepCopy;
     }
 
+
     // TODO appliquer la translation sur la forme.
     public BaseShape translate(Point2d point) {
-        return null;
+        //watch out if point and coords have different dimensions, it doesn't work
+        for(Point2d iter : coords){
+            iter.translate(point);
+        }
+        return this;
     }
+
 
     // TODO appliquer la rotation sur la forme.
     public BaseShape rotate(Double angle) {
-        return null;
+        for(Point2d iter : coords){
+            iter.rotate(angle);
+        }
+        return this;
     }
+
 
     // TODO donner la plus grande valeur en X
     public Double getMaxX() {
-        return null;
+        Double maxValue = Double.MIN_VALUE;
+        for(Point2d iter : coords){
+            if(iter.X() > maxValue){
+                maxValue = iter.X();
+            }
+        }
+        return maxValue;
     }
+
 
     // TODO donner la plus grande valeur en Y
     public Double getMaxY() {
-        return null;
+        Double maxValue = Double.MIN_VALUE;
+        for(Point2d iter : coords){
+            if(iter.Y() > maxValue){
+                maxValue = iter.Y();
+            }
+        }
+        return maxValue;
     }
+
 
     // TODO donner les plus grandes valeurs en X et Y
     public Point2d getMaxCoord() {
-        return null;
+        return new Point2d(getMaxX(), getMaxY());
     }
+
 
     // TODO donner la plus petite valeur en X
     public Double getMinX() {
-        return null;
+        Double minValue = Double.MAX_VALUE;
+        for(Point2d iter : coords){
+            if(iter.X() < minValue){
+                minValue = iter.X();
+            }
+        }
+        return minValue;
     }
+
+
     // TODO donner la plus petite valeur en Y
     public Double getMinY() {
-        return null;
+        Double minValue = Double.MAX_VALUE;
+        for(Point2d iter : coords){
+            if(iter.Y() < minValue){
+                minValue = iter.Y();
+            }
+        }
+        return minValue;
     }
 
     // TODO donner les plus petites valeurs en X et Y
     public Point2d getMinCoord() {
-        return null;
+        return new Point2d(getMinX(), getMinY());
     }
 
     // TODO retourner une nouvelle forme.
